37d41f1a2c11736c3d89c5424f51e1f5b3b8cf17
J03TRESERT <64477826+J03TRESERT@users.noreply.github.com>
2021-07-03 14:18:36 -0400

    Add files via upload

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,6 @@
 let numberselect = 0,
   numbscreen = document.getElementById("number-screen"),
+  numbscreenCache = document.getElementById("number-screen-cache"),
   operator = "",
   numberswrites = "",
   numberswrites2 = "";
@@ -7,9 +8,8 @@ let numberselect = 0,
 let numbers = document.querySelectorAll("#numb-btn").forEach((numbbtn) => {
   numbbtn.addEventListener("click", () => {
     numberselect = Number(numbbtn.textContent);
-    if (numbscreen.textContent <= "0") {
-      numbscreen.textContent = "";
-    }
+    if (numbscreen.textContent <= "0") numbscreen.textContent = "";
+
     numbscreen.textContent += numberselect;
     if (operator == "") numberswrites += numberselect;
     if (operator == "+" || operator == "-" || operator == "*" || operator == "/") {
@@ -18,50 +18,34 @@ let numbers = document.querySelectorAll("#numb-btn").forEach((numbbtn) => {
   });
 });
 
-let operbtn = document.querySelectorAll("#oper-simb").forEach((operbtn) => {
-  operbtn.addEventListener("click", () => {
-    if ((operator == "+" && numberswrites2 == "") || (operator == "-" && numberswrites2 == "") || (operator == "*" && numberswrites2 == "") || (operator == "/" && numberswrites2 == "")) numbscreen.textContent = numbscreen.textContent.slice(0, -1);
-    operator = operbtn.textContent;
-    numbscreen.textContent += operator;
-    numberswrites2 = "";
-  });
+let operbtn = document.querySelector(".operation-simbols");
+operbtn.addEventListener("click", (e) => {
+  if ((operator == "+" && numberswrites2 == "") || (operator == "-" && numberswrites2 == "") || (operator == "*" && numberswrites2 == "") || (operator == "/" && numberswrites2 == "")) numbscreen.textContent = numbscreen.textContent.slice(0, -1);
+  if (e.target.textContent == "+" || e.target.textContent == "-" || e.target.textContent == "/" || e.target.textContent == "*") operator = e.target.textContent;
+  numbscreen.textContent += operator;
+  numberswrites2 = "";
 });
 
-const suma = () => {
-  let resul = Number(numberswrites) + Number(numberswrites2);
-  return resul;
-};
-const resta = () => {
-  let resul = Number(numberswrites) - Number(numberswrites2);
-  return resul;
-};
-const mltp = () => {
-  let resul = Number(numberswrites) * Number(numberswrites2);
-  return resul;
-};
-const dvd = () => {
-  let resul = Number(numberswrites) / Number(numberswrites2);
-  return resul;
-};
+const suma = () => (result = Number(numberswrites) + Number(numberswrites2));
+const resta = () => (resul = Number(numberswrites) - Number(numberswrites2));
+const mltp = () => (resul = Number(numberswrites) * Number(numberswrites2));
+const dvd = () => (resul = Number(numberswrites) / Number(numberswrites2));
 
 let operatorequal = document.querySelector("#oper-simb-equal");
 operatorequal.addEventListener("click", () => {
-  if (operator == "+") {
-    numbscreen.textContent = suma();
-    numberswrites = `${suma()}`;
-  }
-  if (operator == "-") {
-    numbscreen.textContent = resta();
-    numberswrites = `${resta()}`;
-  }
-  if (operator == "*") {
-    numbscreen.textContent = mltp();
-    numberswrites = `${mltp()}`;
-  }
-  if (operator == "/") {
-    numbscreen.textContent = dvd();
-    numberswrites = `${dvd()}`;
-  }
+  numbscreenCache.textContent = "";
+  if (operator == "+") numbscreenCache.textContent = suma();
+
+  if (operator == "-") numbscreenCache.textContent = resta();
+
+  if (operator == "*") numbscreenCache.textContent = mltp();
+
+  if (operator == "/") numbscreenCache.textContent = dvd();
+
+  if (numbscreenCache.textContent == "NaN") numbscreenCache.textContent = "Error";
+
+  numbscreen.textContent = 0;
+  numberswrites = "";
   numberswrites2 = "";
   countnumberscreen = 0;
   operator = "";
